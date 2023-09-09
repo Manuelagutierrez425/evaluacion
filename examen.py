98d0acc07dbd894adf19a87080c4bb99e762ade1
Cristhian Martínez <martinezcuestacris@gmail.com>
2023-09-09 16:12:52 -0500

    jeje

--- examen.py
+++ examen.py
@@ -2,9 +2,9 @@ notas_estudiantes = {
     'Juan': [3.2, 4.5, 5],
     'Maria': [4.2, 3.5, 4.3],
     'Pedro': [3.9, 2.5, 4.8]
-
 }
-
-for estudiante, notas in notas_estudiantes.items():
-    promedio = sum(notas) / len(notas)
-    print(f'El promedio de {estudiante} es: {promedio}')
+for nombre, listasNotas in notas_estudiantes.items():
+    suma = 0
+    for nota in listasNotas:
+        suma = suma + nota
+        print(nombre,suma/len(listasNotas))
